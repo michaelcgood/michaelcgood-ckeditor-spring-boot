6c62dbd81a7f7084fcc2806f8ca66b031097ce09
michaelcgood <31810784+michaelcgood@users.noreply.github.com>
2017-11-14 14:54:22 -0500

    Minor changes to success div for POST request

--- src/main/java/com/michaelcgood/uploader/FileUploadController.java
+++ src/main/java/com/michaelcgood/uploader/FileUploadController.java
@@ -17,7 +17,6 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -76,7 +75,7 @@ public class FileUploadController {
     }
 
     @PostMapping("/")
-    public String postTheFile(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes, Model model)
+    public String postTheFile(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes)
         throws IOException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException {
         // store the file to server
         storageService.store(file);
